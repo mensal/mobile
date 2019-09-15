11248cddf4e61ad8c8bf3cb370a508648639e509
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2019-09-15 18:02:16 -0300

    configuração inicial do projeto

---
+++ src/pages/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+// import styles from './styles';
+
+export default function Main() {
+  return (
+    <View>
+      <Text>X</Text>
+      <Text>xxx</Text>
+    </View>
+  );
+}
