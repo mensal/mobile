326ebb11e94d900c95d454df34b207784a616f11
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2019-09-15 22:56:03 -0300

    colocando o gráfico estático

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,33 +1,113 @@
 import React from 'react';
+import { PieChart } from 'react-native-svg-charts'
 
 import { Text } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
-import { Container, ShotcutContainer, Shortcut } from './styles';
+import {
+  Container,
+  SafeContainer,
+  GraphContainer,
+  HistoryContainer,
+  History,
+  ShortcutContainer,
+  Shortcuts,
+  Shortcut,
+  ShortcutText,
+} from './styles';
+import { State } from 'react-native-gesture-handler';
 
 // <SafeAreaView>
 // </SafeAreaView>
 
-// TODO: Seguir este passo para corrigir o erro no Android:
+// TODO: Seguir estes passos para corrigir os erros no Android:
 // https://github.com/react-native-community/react-native-linear-gradient
+// https://github.com/JesperLekland/react-native-svg-charts
+
+// TODO: Geração de cores harmonizadas:
+// http://colormind.io
 
 export default function Main() {
+  // const data = [1234, 2050];
+
+  // const randomColor = () =>
+  //   ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(
+  //     0,
+  //     7,
+  //   );
+
+  // const pieData = data
+  //   .filter(value => value > 0)
+  //   .map((value, index) => ({
+  //     value,
+  //     svg: {
+  //       fill: randomColor(),
+  //       onPress: () => console.log('press', index),
+  //     },
+  //     key: `pie-${index}`,
+  //   }));
+
+  const pieData = [
+    {
+      value: 1234,
+      svg: {
+        fill: '#D88661',
+      },
+      key: 1,
+    },
+    {
+      value: 2050,
+      svg: {
+        fill: '#C34759',
+      },
+      key: 2,
+    },
+  ];
 
   return (
     // https://uigradients.com/#VeniceBlue
 
-    <Container colors={['#085078', '#85D8CE']}>
-      <SafeAreaView>
-        <Text>X</Text>
-        <Text>Cleverson</Text>
-        <ShotcutContainer>
-          <Shortcut>
-            <Text>X</Text>
-          </Shortcut>
-          <Shortcut>
-            <Text>X</Text>
-          </Shortcut>
-        </ShotcutContainer>
-      </SafeAreaView>
+    <Container colors={['#92FE9D', '#00C9FF']}>
+      <SafeContainer>
+        <GraphContainer>
+          <PieChart
+            style={{ height: 120 }}
+            data={pieData}
+            innerRadius="80%"
+            labelRadius="Teste"
+            animate={true}
+          />
+        </GraphContainer>
+        <HistoryContainer>
+          <History />
+        </HistoryContainer>
+        <ShortcutContainer>
+          <Shortcuts>
+            <Shortcut>
+              <ShortcutText>+</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+            <Shortcut>
+              <ShortcutText>Outro atalho</ShortcutText>
+            </Shortcut>
+          </Shortcuts>
+        </ShortcutContainer>
+      </SafeContainer>
     </Container>
   );
 }
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,33 +1,138 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
+import { StyleSheet, Dimensions } from "react-native";
+
+const shortcutDimension = {height: 135, width: 135};
+const graphHeight = shortcutDimension.height * 1.8;
 
 export const Container = styled(LinearGradient)`
   flex: 1;
-  flex-direction: row;
-  background-color: blue;
-  justify-content: center;
-  margin: 1px;
+  /* flex-direction: row; */
 `;
 
-export const ShotcutContainer = styled.ScrollView.attrs({
-    horizontal: true,
-    showsHorizontalScrollIndicator: false,
-    contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
+export const SafeContainer = styled.SafeAreaView`
+  flex: 1;
+  /* justify-content: flex-end; */
+  /* justify-content: center; */
+  /* flex-direction: column; */
+  /* flex-direction: row; */
+  /* width: 100%; */
+  /* background-color: blue; */
+  /* align-items: stretch; */
+  /* margin: 1px; */
+`;
+
+export const GraphContainer = styled.View`
+  /* flex: 1; */
+  /* border: 1px; */
+  height: ${graphHeight}px;
+  /* align-content: center; */
+  justify-content: center;
+`;
+
+export const HistoryContainer = styled.View`
+  flex: 1;
+`;
+
+export const History = styled.View`
+  /* flex: 1; */
+  /* border: 1px; */
+  /* align-content: center; */
+  background-color: #ffffff;
+  border-radius: 12px;
+  margin-left: 8px;
+  margin-right: 8px;
+  padding: 12px;
+
+  left: 0px;
+  right: 0px;
+  height: ${Dimensions.get('window').height - graphHeight}px;
+
+  box-shadow: 0px 4px 4px #999;
+
+  position: absolute;
+  /* opacity: 0.8; */
+`;
+
+export const ShortcutContainer = styled.View`
+  /* flex: 1; */
+  /* border: 0.1px; */
+
+  /* top: 520px;
+  position: absolute; */
+  /* vertical-align: bottom; */
+  /* margin-left: 8px; */
+  /* margin-right: 8px; */
+  /* background-color: #ffffff; */
+`;
+
+export const Shortcuts = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingLeft: 20, paddingRight: 12 },
 })`
-    /* height: 100px; */
+  /* height: 100px; */
   /* flex: 1; */
   /* background-color: white; */
 
   /* vertical-align: bottom; */
   /* flex: 1; */
-  margin: 1px;
+  /* margin: 1px; */
+  /* border: 1px; */
+  /* align-self: fle */
+  /* width: 100%; */
+  margin-bottom: 8px;
 `;
 
 export const Shortcut = styled.View`
   /* flex: 1; */
-  background-color: red;
-  height: 100px;
-  width: 100px;
+  /* background-color: #9FE5F6; */
+  /* background-color: #fff; */
+  background-color: #72D8D1;
+  /* border-radius: 8px; */
+  /* color: #fff; */
+  height: ${shortcutDimension.height}px;
+  width: ${shortcutDimension.width}px;
+  padding: 5px;
+  /* margin: 4px; */
+  margin-right: 8px;
+
+  border: ${StyleSheet.hairlineWidth}px;
+  /* border: 1px; */
+  /* border-color: #00C9FF */
+  /* border-color: #2eb43b */
+  border-color: #fff
+
+  /* opacity: 0.5; */
   /* justify-content: space-between; */
-  margin: 1px;
+  /* border: 1px; */
+
+  /* shadow-color: #000; */
+  /* shadow-offset: {width: 0, height: 2}; */
+  /* shadow-opacity: 0.8; */
+  /* shadow-radius: 2; */
+  /* elevation: 1; */
+
+  /* box-shadow: 0px 1px 1px #333; */
+
+  /* border-width: 1;
+  border-radius: 2;
+  border-color: #ddd;
+  border-bottom-width: 0;
+  shadow-color: #000;
+  shadow-offset: {width: 0, height: 2};
+  shadow-opacity: 0.8;
+  shadow-radius: 2;
+  elevation: 1;
+  margin-left: 5;
+  margin-right: 5;
+  margin-top: 10; */
+`;
+
+export const ShortcutText = styled.Text`
+  /* color: #00C9FF; */
+  /* color: #2eb43b; */
+  color: #222;
+  font-weight: 100;
+  font-size: 12px;
 `;
